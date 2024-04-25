759a5a0c337a8bcb86d8ee282fdcb7d4eb50ca2c
Michael Aklilu <michaelaklilu@gmail.com>
2024-04-25 12:30:15 +0300

    Create container and div elements using javascript

---
+++ javascript.js
@@ -0,0 +1,16 @@
+const container = document.querySelector("#container");
+
+const firstBox = document.createElement("div");
+container.appendChild(first.Box);
+
+const secondBox = document.createElement("div");
+container.appendChild(secondBox);
+
+const thirdBox = document.createElement("div");
+container.appendChild(thirdBox);
+
+const fourthBox = document.createElement("div");
+container.appendChild(fourthBox);
+
+const fifthBox = document.createElement("div");
+container.appendChild(fifthBox);
