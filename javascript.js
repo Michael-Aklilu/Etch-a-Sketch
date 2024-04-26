f582964d743ebfa3efbd894fb5d937cdc48104c1
Michael Aklilu <michaelaklilu@gmail.com>
2024-04-26 11:48:34 +0300

    Set up hovering effect where if mouse points at box it's color changes

--- javascript.js
+++ javascript.js
@@ -3,11 +3,21 @@ const container = document.querySelector("#container");
 const row = 16;
 const column = 16;
 
+
 for(let i = 0; i<row;i++){
     for(let j = 0; j<column; j++)
     {
-        const gridItem = document.createElement("div");
+        gridItem = document.createElement("div");
+        gridItem.classList.add("box");
         container.appendChild(gridItem);
+        
+        
     }
 }
+const grid = document.querySelectorAll('.box');
+grid.forEach(el => el.addEventListener('mouseenter',(event) => {
+     
+    event.target.setAttribute("style", "background: red;")
+}));
+
 
